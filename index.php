3329a392b5ee45c1cea640b2bafb1d471394dc95
Matteo Magnani <maniax.matteo@gmail.com>
2017-11-23 10:43:04 +0100

    Removing auto slide from video

--- index.php
+++ index.php
@@ -87,7 +87,7 @@ require_once("config.inc.php");
             // [string] Can be either "slide" or "fade".
           interval: 5000,
             // [number] Time spent on each slide in milliseconds.
-          auto: true,
+          auto: false,
             // [boolean] Start playing the slideshow on load.
           swap: true,
             // [boolean] show/hide stop and play buttons
@@ -108,7 +108,7 @@ require_once("config.inc.php");
             // [string] Can be either "slide" or "fade".
           interval: 5000,
             // [number] Time spent on each slide in milliseconds.
-          auto: true,
+          auto: false,
             // [boolean] Start playing the slideshow on load.
           swap: true,
             // [boolean] show/hide stop and play buttons
@@ -134,7 +134,7 @@ require_once("config.inc.php");
                   // You cannot use your own buttons. Sorry.
                 effect: "fade",
                   // [string] Can be either "slide" or "fade".
-                interval: 5000,
+                interval: 6000,
                   // [number] Time spent on each slide in milliseconds.
                 auto: true,
                   // [boolean] Start playing the slideshow on load.
